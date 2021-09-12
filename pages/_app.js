1a15303b580055f7c777f896aa2ffa4e2e75ec59
Adryan Rosa <adryanrosa374@gmail.com>
2021-09-12 13:43:15 -0300

    Adiciona arquivos iniciais sass

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,14 @@
 import Head from 'next/head';
+import '../src/styles/main.scss';
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
       <Head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
+        <link href="https://fonts.googleapis.com/css2?family=Fira+Code:wght@500&family=Open+Sans&display=swap" rel="stylesheet" />
+
         <title>Adryan Rosa | Portfolio</title>
         <link rel="icon" href="/favicon.svg" />
       </Head>
