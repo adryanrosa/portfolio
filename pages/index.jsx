c92d1b67cfcbb7bec6938a102797d1c75ee22e01
Adryan Rosa <adryanrosa374@gmail.com>
2021-09-12 19:10:41 -0300

    Atualiza hover states

--- pages/index.jsx
+++ pages/index.jsx
@@ -28,7 +28,7 @@ export default function Home() {
                 />
               </a>
 
-              <a href="https://www.linkedin.com/in/adryan-rosa" target="_blank" rel="noreferrer">
+              <a href="https://www.linkedin.com/in/adryan-rosa/" target="_blank" rel="noreferrer">
                 <Image
                   src="/icons/linkedin.svg"
                   width={ 24 }
